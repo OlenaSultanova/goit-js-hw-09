70910270758347b0257717feb3917dca15d51e19
OlenaSultanova <galeodeahelen@gmail.com>
2023-08-20 01:41:18 +0300

    done task01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const ChangeColorDelay = 1000;
+let idInterval = null;
 
+const refs = {
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
+
+refs.btnStop.disabled = true;
+refs.btnStart.addEventListener('click', onBtnStartChangeColor);
+refs.btnStop.addEventListener('click', onBtnStopChangeColor);
+
+function onBtnStartChangeColor() {
+  refs.btnStart.disabled = true;
+  refs.btnStop.disabled = false;
+
+  idInterval = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, ChangeColorDelay);
+}
+
+function onBtnStopChangeColor() {
+  refs.btnStart.disabled = false;
+  refs.btnStop.disabled = true;
+
+  clearInterval(idInterval);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
